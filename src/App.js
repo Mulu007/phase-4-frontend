d1af64a1383bc9f5e244b75c81f8c2c8b3fa99a9
Mulu <silvanius.mulu@student.moringaschool.com>
2022-12-06 12:10:26 +0300

    Addition of signup and login file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 // import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import Navbar from "./components/Navbar";
+import Signup from "./components/Signup";
 
 function App() {
   return (
     <div>
       <Navbar/>
-
+      <Routes>
+      {/* <Route path="/" element={<Home/>}></Route> */}
+      <Route path="/login" element={<Login/>}></Route>
+      <Route path="/signup" element={<Signup/>}></Route>
+      </Routes>
       {/* <Footer/> */}
     </div>
   );
