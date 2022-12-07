31e7a113b4d7f5e77f5858f877bf0fee6ac5a7ba
Mulu <silvanius.mulu@student.moringaschool.com>
2022-12-07 12:05:58 +0300

    Addition of left and right click button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,10 +6,10 @@ const Home = () => {
   return (
     <div>
       <Main/>
-      <Row title="Up coming" fetchURL="http://localhost:3000/movies"/>
-      <Row title="Popular" fetchURL="http://localhost:3000/movies"/>
-      <Row title="Trending" fetchURL="http://localhost:3000/movies"/>
-      <Row title="Top Rated" fetchURL="http://localhost:3000/movies"/>
+      <Row rowID="1" title="Up coming" fetchURL="http://localhost:3000/movies"/>
+      <Row rowID="2" title="Popular" fetchURL="http://localhost:3000/movies"/>
+      <Row rowID="3" title="Trending" fetchURL="http://localhost:3000/movies"/>
+      <Row rowID="4" title="Top Rated" fetchURL="http://localhost:3000/movies"/>
     </div>
   )
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Movie from './Movie'
+import {MdChevronLeft, MdChevronRight} from "react-icons/md"
 
-const Row = ({title, fetchURL}) => {
+const Row = ({title, fetchURL, rowID}) => {
     const [images, setImages] = useState([])
 
     useEffect(() => {
@@ -16,16 +17,29 @@ const Row = ({title, fetchURL}) => {
         setImages(data)
     );
     }
+
+
+    const slideLeft = () => {
+        var slider = document.getElementById('slider' + rowID)
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+
+    const slideRight = () => {
+        var slider = document.getElementById('slider' + rowID)
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
       
   return (
     <div>
        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
        <div className='relative flex items-center group'>
-        <div id={'slider'} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
+        <MdChevronLeft onClick={slideLeft} size={40} className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
+        <div id={'slider' + rowID} className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative">
             {images.map((item,id) => (
                 <Movie key={id} item={item}/>
             ))}
         </div>
+        <MdChevronRight onClick={slideRight} size={40} className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"/>
        </div> 
     </div>
   )
