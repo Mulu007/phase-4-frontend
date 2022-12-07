b07f7ea414fa503a8b35798c897bc3ad5cd47721
Mulu <silvanius.mulu@student.moringaschool.com>
2022-12-07 12:57:37 +0300

    Editing sign up form

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Signup = () => {
+    const [name, setName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [avatarurl, setAvatarurl] = useState("")
     let navigate = useNavigate()
 
     const handleSubmit = async (e) => {
@@ -26,8 +28,10 @@ const Signup = () => {
                     <div className='max-w-[320px] mx-auto py-16'>
                         <h1 className='text-3xl font-bold'>Sign Up</h1>
                         <form onSubmit={handleSubmit} className='w-full flex flex-col py-4'>
+                        <input onChange={(e) => setName(e.target.value)}className="p-3 my-2 bg-gray-700 rounded" type="text" placeholder="Name"/>
                             <input onChange={(e) => setEmail(e.target.value)} className="p-3 my-2 bg-gray-700 rounded" type="email" placeholder='Email' autoComplete='email'/>
                             <input onChange={(e) => setPassword(e.target.value)}className="p-3 my-2 bg-gray-700 rounded" type="password" placeholder="Password" autoComplete='current-password'/>
+                            <input onChange={(e) => setAvatarurl(e.target.value)}className="p-3 my-2 bg-gray-700 rounded" type="text" placeholder="Avatar Url"/>
                             <button className='bg-red-600 py-3 my-6 rounded font-bold'>Sign Up</button>
                             <div className='flex justify-between items-center text-sm text-gray-600'>
                                 <p><input className='mr-2' type="checkbox"/>Remember me</p>
